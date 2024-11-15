887e394be514c2fdee49f406266cb54c68adff51
mohana <mohana.priya@gavstech.com>
2024-11-15 13:34:19 +0530

    wallet connection

--- lib/module/wallet_connection/wallet_connection_controller.dart
+++ lib/module/wallet_connection/wallet_connection_controller.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_state_manager/get_state_manager.dart';
-import 'package:url_launcher/url_launcher.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 import 'package:walletconnect_dart/walletconnect_dart.dart';
 
